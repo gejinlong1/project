51e366dbd79dcd9182f8772bf64fc60ff5cf7f82
gejinlong1 <2811802441@qq.com>
2021-06-07 10:10:27 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import $echarts from 'echarts'
+
 
 Vue.config.productionTip = false
+Vue.use(axios)
+Vue.use(ElementUI)
+Vue.prototype.$echarts = $echarts
+
+
 
 new Vue({
   router,
